96b2426433a7a54601f4ff8c71c6e9423e279674
Julie Dujardin <julie.dujardin@harfanglab.fr>
2024-10-17 18:32:55 +0200

    input: use godot input map to trigger canon

--- scripts/canon.gd
+++ scripts/canon.gd
@@ -5,18 +5,13 @@ const Ammo := preload("res://scenes/ammo.tscn")
 @onready var spawn = $Marker3D
 
 func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed:
-			match event.keycode:
-				# TODO: change key to mouse
-				KEY_A:
-					# TODO: play shoot sfx
-
-					var ammo := Ammo.instantiate()
-					get_owner().get_parent().add_child(ammo)
-					ammo.global_position = spawn.global_position
-					
-					var target: Vector3 = to_global(spawn.position + Vector3.UP)
-					var dir: Vector3 = spawn.global_position - target
-					var speed := 15
-					ammo.apply_impulse(dir.normalized() * speed)
+	if event.is_action_pressed(&"shoot"):
+		# TODO: play shoot sfx
+		var ammo := Ammo.instantiate()
+		get_owner().get_parent().add_child(ammo)
+		ammo.global_position = spawn.global_position
+		
+		var target: Vector3 = to_global(spawn.position + Vector3.UP)
+		var dir: Vector3 = spawn.global_position - target
+		var speed := 15
+		ammo.apply_impulse(dir.normalized() * speed)
