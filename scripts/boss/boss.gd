2403af136b077cef45ce25e77b3029d5acaf1bc7
florian <florian@harfanglab.fr>
2024-10-18 18:27:39 +0200

    improve pattern system

--- scripts/boss/boss.gd
+++ scripts/boss/boss.gd
@@ -2,7 +2,9 @@ class_name Boss
 extends RigidBody3D
 
 var Patterns: Array[Pattern] = [
+	# Charge (maybe made AoE?)
 	preload("res://scenes/pattern/stun_attack.tscn").instantiate(),
+	preload("res://scenes/pattern/stun_attack_variation.tscn").instantiate(),
 	preload("res://scenes/pattern/forward_attack.tscn").instantiate(),
 ]
 
@@ -13,12 +15,13 @@ enum State {NOOP, SETUP_PATTERN, ATTACK}
 var current_pattern: Pattern
 var pattern_cooldown: float = 0
 var state: State
-var current_target: Vector3
+var pattern_transform: Transform3D
 
 @onready var weakpoint: Weakpoint = $Weakpoint
 @onready var blink_component = $BlinkComponent
 @onready var life_component = $LifeComponent
-@onready var player = get_tree().get_nodes_in_group(&"player")[0]
+@onready var player: Node3D = get_tree().get_nodes_in_group(&"player")[0]
+@onready var debug_state_label: Label3D = $DebugStateLabel
 
 
 func _ready():
@@ -34,7 +37,7 @@ func hit(damage: int) -> void:
 	set_state(State.NOOP)
 	pattern_cooldown = 4.0
 	
-	apply_central_impulse((global_position - current_target).normalized() * 20)
+	apply_central_impulse((global_position - player.global_position).normalized() * 20)
 	
 	# TODO: play hit sfx
 	print("hit")
@@ -46,13 +49,29 @@ func set_state(new_state):
 		state = State.ATTACK
 		pattern_cooldown = 0
 		linear_velocity = Vector3.ZERO
-		current_target = player.global_position
+		
+		# Update the matrix to apply to the pattern
+		var current_target := player.global_position
+		current_target.y = 0
+		pattern_transform.origin = current_target
+
+		# Made the boss looking to the player
+		var dest := current_pattern.end_pos(pattern_transform)
+		look_at(Vector3(dest.x, global_position.y, dest.z), Vector3.UP, true)
 		
 	elif new_state == State.SETUP_PATTERN: 
 		state = State.SETUP_PATTERN
 		current_pattern = Patterns[randi() % Patterns.size()]
-		current_target = player.global_position
+		
+		# Compute the "world matrix" to apply to the pattern
+		# This is usefull to always focus the player and made some variation with the rotation
+		var current_target := player.global_position
 		current_target.y = 0
+		pattern_transform = Transform3D(Basis(Vector3.UP, randf_range(0, TAU)), current_target)
+		
+		# Made the boss looking to the player
+		var dest := current_pattern.start_pos(pattern_transform)
+		look_at(Vector3(dest.x, global_position.y, dest.z), Vector3.UP, true)
 
 	# set new state to NOOP
 	elif new_state == State.NOOP: 
@@ -65,20 +84,28 @@ func set_state(new_state):
 func _process(delta):
 	if not enabled:
 		return
+		
+	if debug_state_label != null:
+		if state == State.ATTACK: 
+			debug_state_label.set_text("Attack (%s)" % current_pattern.pattern_name)
+		elif state == State.SETUP_PATTERN: 
+			debug_state_label.set_text("Prepare Attack (%s)" % current_pattern.pattern_name)
+		else: 
+			debug_state_label.set_text("NOOP")
 	
 	if state == State.NOOP:
 		if pattern_cooldown > 0.0:
 			set_state(State.SETUP_PATTERN)
 
 	elif state == State.SETUP_PATTERN:
-		if global_position.distance_to(current_pattern.start_pos(current_target)) < 2.5:
+		if global_position.distance_to(current_pattern.start_pos(pattern_transform)) < 2.5:
 			set_state(State.ATTACK)
-			
-		apply_central_impulse((current_pattern.start_pos(current_target) - global_position).normalized() * delta * 10)
+
+		apply_central_impulse((current_pattern.start_pos(pattern_transform) - global_position).normalized() * delta * 10)
 
 	elif state == State.ATTACK:
 		# apply the pattern
-		apply_central_impulse(current_pattern.attack_direction(current_target).normalized() * delta * 10)
+		apply_central_impulse(current_pattern.attack_direction(pattern_transform).normalized() * delta * 10)
 		
 		if pattern_cooldown > 2.5:
 			set_state(State.NOOP)
--- scripts/pattern.gd
+++ scripts/pattern.gd
@@ -2,17 +2,18 @@ class_name Pattern
 extends Node3D
 
 
+@export var pattern_name: String
 @export var start: Marker3D
 @export var end: Marker3D
 
 
-func attack_direction(player_pos: Vector3) -> Vector3:
-	return end_pos(player_pos) - start_pos(player_pos)
+func attack_direction(world: Transform3D) -> Vector3:
+	return end_pos(world) - start_pos(world)
 
 
-func start_pos(player_pos: Vector3):
-	return player_pos + start.position * 2
+func start_pos(world: Transform3D) -> Vector3:
+	return world * start.position
 
 
-func end_pos(player_pos: Vector3):
-	return player_pos + end.position * 2
+func end_pos(world: Transform3D) -> Vector3:
+	return world * end.position
