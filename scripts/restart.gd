601c960aa86d96d6f6682aaf9e3c576afcd5938f
florianvazelle <ponythugflorian@gmail.com>
2024-10-26 15:16:55 +0200

    add loading screen with advices

--- scripts/restart.gd
+++ scripts/restart.gd
@@ -7,6 +7,6 @@ func _unhandled_input(event):
 			match event.keycode:
 				KEY_R:
 					get_tree().paused = false
-					get_tree().change_scene_to_file("res://scenes/main.tscn")
+					Loading.load_scene("res://scenes/main.tscn", true)
 				KEY_ESCAPE:
 					get_tree().quit()
